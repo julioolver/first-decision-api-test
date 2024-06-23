9778b7d5ea0fd2035948940d062096b88f599e24
Julio Cesar <julioghost1@gmail.com>
2024-06-23 20:24:21 -0300

    feat: created a TDD to creating user in API, feat #1

--- src/tests/Feature/UserTest.php
+++ src/tests/Feature/UserTest.php
@@ -2,19 +2,37 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 
-class ExampleTest extends TestCase
+class UserTest extends TestCase
 {
     use RefreshDatabase;
+
     /**
      * A basic test example.
      */
     public function testShouldBeAbleToCreateUserWithSuccess(): void
     {
+        $user= User::factory()->create();
+        $userPayload = $user->toArray();
 
-        $this->assertTrue(true);
+        $response = $this->postJson('/api/users', $userPayload);
+        
+        $this->assertDatabaseHas('users', [
+            'name' => $user->name,
+            'email' => $user->email,
+        ]);
+        $response->assertStatus(201);
+        $response->assertJson($userPayload);
+
+        $response->assertJsonStructure([
+            'data' => [
+                'name',
+                'email',
+            ]
+        ]);
     }
 }
